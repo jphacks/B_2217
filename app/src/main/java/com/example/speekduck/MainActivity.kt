fd43205a5d1674f7b83a49bc2143979dc813b6d0
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2022-10-15 17:11:50 +0900

    Update manifestFile & Permission

--- app/src/main/java/com/example/speekduck/MainActivity.kt
+++ app/src/main/java/com/example/speekduck/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import androidx.core.content.ContextCompat
 import android.Manifest.permission.RECORD_AUDIO
 import android.content.pm.PackageManager
+import android.speech.SpeechRecognizer
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+    private var speechRecognizer : SpeechRecognizer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +19,30 @@ class MainActivity : AppCompatActivity() {
         if (granted != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO),1)
         }
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(applicationContext)
+        //speechRecognizer?.setRecognitionListener(createRecognitionListenerStringStream { recognize_text_view.text = it })
+    }
+    /*
+    private fun createRecognitionListenerStringStream(onResult: (String)->Unit):RecognitionListener{
+        return object : RecognitionListener {
+            override fun onRmsChanged(rmsdB: Float) { /** 今回は特に利用しない */ }
+            override fun onReadyForSpeech(params: Bundle) { onResult("onReadyForSpeech") }
+            override fun onBufferReceived(buffer: ByteArray) { onResult("onBufferReceived") }
+            override fun onPartialResults(partialResults: Bundle) { onResult("onPartialResults") }
+            override fun onEvent(eventType: Int, params: Bundle) { onResult("onEvent") }
+            override fun onBeginningOfSpeech() { onResult("onBeginningOfSpeech") }
+            override fun onEndOfSpeech() { onResult("onEndOfSpeech") }
+            override fun onError(error: Int) { onResult("onError") }
+            override fun onResults(results: Bundle) {
+                val stringArray = results.getStringArrayList(android.speech.SpeechRecognizer.RESULTS_RECOGNITION);
+                onResult("onResults " + stringArray.toString())
+            }
+        }
+    }
+     */
+
+    override fun onDestroy() {
+        super.onDestroy()
+        speechRecognizer?.destroy()
     }
 }
