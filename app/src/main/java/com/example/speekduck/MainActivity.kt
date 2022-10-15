97f8ee75ba2d5cae1ae965e78f8bbe0681456135
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2022-10-15 16:01:42 +0900

    Update manifestFile & Permission

--- app/src/main/java/com/example/speekduck/MainActivity.kt
+++ app/src/main/java/com/example/speekduck/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.speekduck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import android.Manifest.permission.RECORD_AUDIO
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val granted = ContextCompat.checkSelfPermission(/* context = */ this, /* permission = */
+            RECORD_AUDIO)
+        if (granted != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO),1)
+        }
     }
 }
