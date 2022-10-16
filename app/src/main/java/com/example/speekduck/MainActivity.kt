f5b3d099147666a7b8e32ce2da6cabd7d02c4ea0
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2022-10-16 11:44:35 +0900

    speech recognize ver.1

--- app/src/main/java/com/example/speekduck/MainActivity.kt
+++ app/src/main/java/com/example/speekduck/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.speekduck
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.core.content.ContextCompat
 import android.Manifest.permission.RECORD_AUDIO
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private var speechRecognizer : SpeechRecognizer? = null
@@ -20,10 +25,13 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO),1)
         }
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(applicationContext)
-        //speechRecognizer?.setRecognitionListener(createRecognitionListenerStringStream { recognize_text_view.text = it })
+        val textMessage = findViewById<TextView>(R.id.textView)
+        speechRecognizer?.setRecognitionListener(createRecognitionListenerStringStream { textMessage.text = it })
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{speechRecognizer?.startListening(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH))}
     }
-    /*
-    private fun createRecognitionListenerStringStream(onResult: (String)->Unit):RecognitionListener{
+
+    private fun createRecognitionListenerStringStream(onResult: (String)->Unit): RecognitionListener {
         return object : RecognitionListener {
             override fun onRmsChanged(rmsdB: Float) { /** 今回は特に利用しない */ }
             override fun onReadyForSpeech(params: Bundle) { onResult("onReadyForSpeech") }
@@ -34,12 +42,12 @@ class MainActivity : AppCompatActivity() {
             override fun onEndOfSpeech() { onResult("onEndOfSpeech") }
             override fun onError(error: Int) { onResult("onError") }
             override fun onResults(results: Bundle) {
-                val stringArray = results.getStringArrayList(android.speech.SpeechRecognizer.RESULTS_RECOGNITION);
-                onResult("onResults " + stringArray.toString())
+                val stringArray = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                onResult(stringArray.toString())
             }
         }
     }
-     */
+
 
     override fun onDestroy() {
         super.onDestroy()
